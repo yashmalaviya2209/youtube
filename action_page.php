5b45ba107df8f30bb9b9a67d041e5e7cbb2dba26
Yash Malaviya <66691142+yashmalaviya2209@users.noreply.github.com>
2023-08-19 13:37:37 +0530

    index and action page

---
+++ action_page.php
@@ -0,0 +1,30 @@
+
+<html>
+<body>    
+<?php
+
+$link =  $_POST['fname'];
+echo $link;
+
+$url = "https://www.youtube.com/embed/$link?autoplay=1&mute=1";
+?>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+</body>
+</html>
