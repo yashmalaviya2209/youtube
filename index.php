d1d2002cc774911330897e6d5a9370d202d21a77
Yash Malaviya <66691142+yashmalaviya2209@users.noreply.github.com>
2023-08-19 13:42:58 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>The input element</h1>
+<?php
+$url = "https://www.youtube.com/embed/5fYUYRUIVUc?autoplay=1&mute=1";
+?>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+<iframe width="420" height="345" src="<?php echo $url; ?>">
+</iframe>
+
+</body>
+</html>
